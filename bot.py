0e2f163bc61b892ffff8eec4289bd525cf7e1410
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2021-02-04 12:10:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, response))
 
     # log all errors
     dp.add_error_handler(error)
