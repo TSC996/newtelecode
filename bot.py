c4d8553ec486280a1405e94c00c703a9d8bbb6d0
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2021-02-07 15:20:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '1400398989:AAEiH29IFAVWdJqylU1lxgN1ev7Kbw3GlzM'
+TOKEN = '1403865219:AAHju1lmn-faKj4u3c8tN3GsYpPY33unuHI'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
